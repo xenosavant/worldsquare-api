9394027845cb938d4f15698e0883bf1c1bdaab5e
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2018-05-19 01:52:01 -0700

    Services/HorizonService: add create account feature
    Horizon_Server() - Add static method which will create
    account and fill the keypair data object.
    Create_Stellar_account() - Add method to call test or public network.
    
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- src/Services/HorizonService.cs
+++ src/Services/HorizonService.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using stellar_dotnetcore_sdk;
+using Horizon_data = Stellmart.Api.Data.Horizon;
 
 namespace Stellmart.Services
 {
     public class HorizonService
     {
+        static public Server server;
+        public static void Horizon_Server(string network) {
+            if(network.Equals("public")) {
+                Network.UsePublicNetwork();
+                server = new Server("https://horizon.stellar.org");
+            } else {
+                Network.UseTestNetwork();
+                server = new Server("https://horizon-testnet.stellar.org");
+            }
+        }
+        public static void Create_Stellar_account(ref Horizon_data.KeyPair data) {
+            var keypair = KeyPair.Random();
+            data.Public_Key = keypair.AccountId;
+	    /* ToDo encode the SecretSeed */
+            data.Encoded_Secret = keypair.SecretSeed;
+        }
     }
 }
