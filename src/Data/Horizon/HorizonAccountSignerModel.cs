cb2ac7c196d745d505b9b5a0457b4045fd5b69b4
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2018-06-09 02:31:25 -0700

    Data:Horizon: Add account weight and threshold data structure
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

---
+++ src/Data/Horizon/HorizonAccountSignerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Stellmart.Api.Data.Horizon
+{
+    public class HorizonAccountSignerModel
+    {
+        public string PublicKey { get; set; }
+        public int Weight { get; set; }
+    }
+}
