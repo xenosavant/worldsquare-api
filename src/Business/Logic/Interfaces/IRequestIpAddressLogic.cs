b077186a1cb12dea6c9cd93a6817b6ef5c763fa9
Amel Salibasic <nukecx@gmail.com>
2018-09-12 21:56:50 +0100

    request ip address logic

---
+++ src/Business/Logic/Interfaces/IRequestIpAddressLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace Stellmart.Api.Business.Logic.Interfaces
+{
+    /// <summary>
+    ///     Manager for accessing IP Addresses from an Http Request
+    /// </summary>
+    public interface IRequestIpAddressManager
+    {
+        /// <summary>
+        ///     Gets the IP Address of the client from the request, inspecting any proxy headers as appropriate.
+        /// </summary>
+        /// <param name="httpContext">the http context.</param>
+        /// <returns>The IP Address.</returns>
+        IPAddress Get(HttpContext httpContext);
+    }
+}
---
+++ src/Business/Logic/RequestIpAddressLogic.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Stellmart.Api.Business.Logic.Interfaces;
+using System.Net;
+
+namespace Stellmart.Api.Business.Logic
+{
+    public class RequestIpAddressLogic : IRequestIpAddressLogic
+    {
+        // format is - X-Forwarded-For: client, proxy1, proxy2[3]
+        private const string ForwardedFor = "X-Forwarded-For";
+
+        public IPAddress Get(HttpContext httpContext)
+        {
+            // no http context, therefore no address!
+            if (httpContext == null)
+            {
+                return null;
+            }
+
+            // check for the ForwardedFor header
+            if (httpContext.Request.Headers.ContainsKey(ForwardedFor))
+            {
+                string forwardedFor = httpContext.Request.Headers[ForwardedFor];
+
+                if (!string.IsNullOrEmpty(forwardedFor))
+                {
+                    if (IPAddress.TryParse(forwardedFor.Split(',')[0], out IPAddress clientAddress))
+                    {
+                        return clientAddress;
+                    }
+                }
+            }
+
+            return httpContext.Connection?.RemoteIpAddress;
+        }
+    }
+}
