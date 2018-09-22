2c6c487557caee9e8d6be5833fe541c9182790bf
Amel Salibasic <nukecx@gmail.com>
2018-09-22 19:57:19 +0100

    country data manager

---
+++ src/Business/Managers/CountryDataManager.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Stellmart.Api.Business.Managers.Interfaces;
+using Stellmart.Api.Context.Entities;
+using Stellmart.Api.DataAccess;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Stellmart.Api.Business.Managers
+{
+    public class CountryDataManager : ICountryDataManager
+    {
+        private readonly IRepository _repository;
+
+        public CountryDataManager(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IReadOnlyCollection<Country>> GetAllAsync()
+        {
+            return await _repository.MinimalGetAllAsync<Country>() as IReadOnlyCollection<Country>;
+        }
+
+        public async Task<Country> GetByIsoAsync(string countryIso)
+        {
+            return await _repository.MinimalGetOneAsync<Country>(x => x.Code == countryIso);
+        }
+    }
+}
---
+++ src/Business/Managers/Interfaces/ICountryDataManager.cs
@@ -0,0 +1,12 @@
+﻿using Stellmart.Api.Context.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Stellmart.Api.Business.Managers.Interfaces
+{
+    public interface ICountryDataManager
+    {
+        Task<IReadOnlyCollection<Country>> GetAllAsync();
+        Task<Country> GetByIsoAsync(string countryIso);
+    }
+}
