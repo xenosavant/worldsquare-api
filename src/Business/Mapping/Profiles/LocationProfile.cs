20c601b06745d38f4d6ea34ea1e3e7afc7c3c461
Amel Salibasic <nukecx@gmail.com>
2018-10-09 00:11:44 +0100

    location mapping

---
+++ src/Business/Mapping/Profiles/LocationProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Stellmart.Api.Context;
+using Stellmart.Api.Data;
+
+namespace Stellmart.Api.Business.Mapping.Profiles
+{
+    public class LocationProfile : Profile
+    {
+        public LocationProfile()
+        {
+            CreateMap<LocationModel, Location>()
+                .ForMember(dest => dest.Address, opts => opts.MapFrom(src => src.Address))
+                .ForMember(dest => dest.LocationComponentsFromApp, opts => opts.MapFrom(src => src.LocationComponentsFromApp))
+                .ForMember(dest => dest.LocationComponentsFromGoogleApi, opts => opts.MapFrom(src => src.LocationComponentsFromGoogleApi))
+                .ForMember(dest => dest.IsActive, opts => opts.MapFrom(src => src.IsActive))
+                .ForAllOtherMembers(x => x.Ignore());
+
+            CreateMap<Location, LocationModel>()
+                .ForMember(dest => dest.Address, opts => opts.MapFrom(src => src.Address))
+                .ForMember(dest => dest.LocationComponentsFromApp, opts => opts.MapFrom(src => src.LocationComponentsFromApp))
+                .ForMember(dest => dest.LocationComponentsFromGoogleApi, opts => opts.MapFrom(src => src.LocationComponentsFromGoogleApi))
+                .ForMember(dest => dest.IsActive, opts => opts.MapFrom(src => src.IsActive))
+                .ForAllOtherMembers(x => x.Ignore());
+        }
+    }
+}
