73b2bea09c1e3b03158d3174d7bafe6fb8e5a330
Amel Salibasic <nukecx@gmail.com>
2018-10-11 00:15:53 +0100

    creating location

--- src/Business/Managers/LocationDataManager.cs
+++ src/Business/Managers/LocationDataManager.cs
@@ -1,10 +1,13 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using Stellmart.Api.Business.Managers.Interfaces;
 using Stellmart.Api.Context;
 using Stellmart.Api.Data;
 using Stellmart.Api.DataAccess;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Stellmart.Api.Context.Entities;
+using Stellmart.Api.Data.Enums;
 
 namespace Stellmart.Api.Business.Managers
 {
@@ -26,8 +29,34 @@ namespace Stellmart.Api.Business.Managers
 
         public async Task CreateAsync(LocationModel model, int userId)
         {
-            _repository.Create(_mapper.Map<Location>(model), userId);
-            await _repository.SaveAsync();
+            using (var transaction = await _repository.BeginTransactionAsync())
+            {
+                try
+                {
+                    var geolocation = new GeoLocation
+                    {
+                        Latitude = model.Latitude,
+                        Longitude = model.Longtitude
+                    };
+
+                    _repository.Create(geolocation, userId);
+
+                    var location = _mapper.Map<Location>(model);
+                    location.GeoLocation = geolocation;
+                    location.UserId = userId;
+
+                    _repository.Create(location, userId);
+
+                    await _repository.SaveAsync();
+                    transaction.Commit();
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    throw new Exception(e.Message);
+                }
+            }
+            
         }
     }
 }
--- src/Data/LocationModel.cs
+++ src/Data/LocationModel.cs
@@ -5,6 +5,8 @@
         public string Address { get; set; }
         public string LocationComponentsFromApp { get; set; }
         public string LocationComponentsFromGoogleApi { get; set; }
+        public double Latitude { get; set; }
+        public double Longtitude { get; set; }
         public bool IsActive { get; set; }
     }
 }
