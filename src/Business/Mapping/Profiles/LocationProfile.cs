ba2e12b62493354c8f57f04595bd621cbe881f42
Amel Salibasic <nukecx@gmail.com>
2018-10-14 21:58:41 +0100

    location profile update with isDefault property

--- src/Business/Mapping/Profiles/LocationProfile.cs
+++ src/Business/Mapping/Profiles/LocationProfile.cs
@@ -9,17 +9,21 @@ namespace Stellmart.Api.Business.Mapping.Profiles
         public LocationProfile()
         {
             CreateMap<LocationModel, Location>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Address, opts => opts.MapFrom(src => src.Address))
                 .ForMember(dest => dest.LocationComponentsFromApp, opts => opts.MapFrom(src => src.LocationComponentsFromApp))
                 .ForMember(dest => dest.LocationComponentsFromGoogleApi, opts => opts.MapFrom(src => src.LocationComponentsFromGoogleApi))
                 .ForMember(dest => dest.IsActive, opts => opts.MapFrom(src => src.IsActive))
+                .ForMember(dest => dest.IsDefault, opts => opts.MapFrom(src => src.IsDefault))
                 .ForAllOtherMembers(x => x.Ignore());
 
             CreateMap<Location, LocationModel>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Address, opts => opts.MapFrom(src => src.Address))
                 .ForMember(dest => dest.LocationComponentsFromApp, opts => opts.MapFrom(src => src.LocationComponentsFromApp))
                 .ForMember(dest => dest.LocationComponentsFromGoogleApi, opts => opts.MapFrom(src => src.LocationComponentsFromGoogleApi))
                 .ForMember(dest => dest.IsActive, opts => opts.MapFrom(src => src.IsActive))
+                .ForMember(dest => dest.IsDefault, opts => opts.MapFrom(src => src.IsDefault))
                 .ForAllOtherMembers(x => x.Ignore());
         }
     }
