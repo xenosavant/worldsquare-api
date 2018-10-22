514a8fba88e73d0f4cd56f8f913de200192f089e
Amel Salibasic <nukecx@gmail.com>
2018-10-22 01:02:38 +0100

    change password and reset password models

---
+++ src/Data/Account/ChangePasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Stellmart.Api.Data.Account
+{
+    public class ChangePasswordRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/Data/Account/ResetPasswordRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stellmart.Api.Data.Account
+{
+    /// <summary>
+    /// Reset password model - represents input for reset passsword form for user
+    /// </summary>
+    public class ResetPasswordRequest
+    {
+        /// <summary>
+        /// Represents id of user to reset password
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// Represents reset password form's email address input
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Represents reset password form's confirm email input
+        /// </summary>
+        public string ConfirmPassword { get; set; }
+        /// <summary>
+        /// Represents reset password code
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
