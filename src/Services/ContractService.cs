8ae48e32dfe64e85d7773f4375e957afbe8deb66
Jagadish <jagdish.krishna@gmail.com>
2018-11-04 00:48:01 -0700

    Contract: Use Contract entity
    Add Contract Fund
    Deprecate Contract data model (TBD to remove it)
    
    Signed-off-by: Jagadish <jagdish.krishna@gmail.com>

--- src/Services/ContractService.cs
+++ src/Services/ContractService.cs
@@ -12,66 +12,74 @@ namespace Stellmart.Services
     public class ContractService : IContractService
     {
 	private readonly IHorizonService _horizon;
-	public ContractModel Contract {get; private set;}
-
+	private  HorizonKeyPairModel WorldSquareAccount;
 	public ContractService(IHorizonService horizon)
 	{
 		_horizon = horizon;
 	}
-	//TBD: Add WorldSquare account as signer
-	//TBD: Add async and await
-	public async Task<int> SetupContract(ContractParamModel ContractParam)
+
+	public async Task<Contract> SetupContract(ContractParamModel ContractParam)
 	{
-		if (ContractParam.Type != ContractPreTnxType.Setup)
-			return 0;
+		var ContractSetup = new Contract();
+
+		HorizonKeyPairModel escrow = _horizon.CreateAccount();
+		ContractSetup.EscrowAccountId = escrow.PublicKey;
+		ContractSetup.CurrentSequenceNumber = await _horizon.GetSequenceNumber(escrow.PublicKey);
+		ContractSetup.ContractStateId = (int)ContractState.Initial;
+		ContractSetup.ContractTypeId = 0;
+		return ContractSetup;
+	}
+	public async Task<Contract> FundContract(ContractParamModel ContractParam)
+	{
+		var ContractFund = new Contract();
+
 		HorizonAccountWeightModel weight = new HorizonAccountWeightModel();
 		HorizonAccountSignerModel dest_account = new HorizonAccountSignerModel();
 		HorizonAccountSignerModel ws_account = new HorizonAccountSignerModel();
 		weight.Signers = new List<HorizonAccountSignerModel>();
-		Contract.Txn = new List<SubmitTransactionResponse>();
 		var ops = new List<Operation>();
-		//Create Escrow Account
-		HorizonKeyPairModel escrow = _horizon.CreateAccount();
 		//Transfer funds tp Escrow
         //TBD: consider other assets too
 		//TBD: transfer 1 % to WorldSquare 
-		var PaymentOp = _horizon.CreatePaymentOps(ContractParam.SourceAccount, escrow.PublicKey,
+		var PaymentOp = _horizon.CreatePaymentOps(ContractParam.SourceAccount, ContractParam.EscrowAccount.PublicKey,
                 ContractParam.Asset.Amount);
 		ops.Add(PaymentOp);
 		var txnxdr = await _horizon.CreateTxn(ContractParam.SourceAccount, ops, null);
-		Contract.Txn.Add(await _horizon.SubmitTxn(_horizon.SignTxn(escrow, txnxdr)));
+		await _horizon.SubmitTxn(_horizon.SignTxn(ContractParam.SourceAccount, txnxdr));
 		//clear ops
 		ops.Clear();
 		//Escrow threshold weights are 4
-		weight.LowThreshold = 4;
-		weight.MediumThreshold = 4;
-		weight.HighThreshold = 4;
+		weight.LowThreshold = 5;
+		weight.MediumThreshold = 5;
+		weight.HighThreshold = 7;
 		//escrow master weight (1) + dest weight (1) + WorldSquare (2)
 		//dest account has weight 1
 		dest_account.Signer = ContractParam.DestAccount;
 		dest_account.Weight = 1;
 		weight.Signers.Add(dest_account);
 		//Make sure that WorldSquare weight is added as 2
-		ws_account.Signer = Contract.WorldSquareAccount.PublicKey;
-		ws_account.Weight = 2;
+		ws_account.Signer = WorldSquareAccount.PublicKey;
+		ws_account.Weight = 4;
 		weight.Signers.Add(ws_account);
 		//Let the SignerSecret be null
 		weight.SignerSecret = null;
 
-		var SetOptionsOp = _horizon.SetOptionsOp(escrow, weight);
+		var SetOptionsOp = _horizon.SetOptionsOp(ContractParam.EscrowAccount, weight);
 		ops.Add(SetOptionsOp);
 
-		txnxdr = await _horizon.CreateTxn(escrow, ops, null);
-        Contract.Txn.Add(await _horizon.SubmitTxn(_horizon.SignTxn(escrow, txnxdr)));
-		Contract.Sequence = await _horizon.GetSequenceNumber(escrow.PublicKey);
-		Contract.EscrowAccount = escrow;
-		Contract.DestAccount = ContractParam.DestAccount;
-		Contract.CurrentState = ContractState.Initial;
-		Contract.CurrentPhase = ContractPhaseType.NoPhase;
-		return 1;
+		txnxdr = await _horizon.CreateTxn(ContractParam.EscrowAccount, ops, null);
+        await _horizon.SubmitTxn(_horizon.SignTxn(ContractParam.EscrowAccount, txnxdr));
+
+		ContractFund.EscrowAccountId = ContractParam.EscrowAccount.PublicKey;
+		ContractFund.CurrentSequenceNumber = await _horizon.GetSequenceNumber(ContractParam.EscrowAccount.PublicKey);
+		ContractFund.ContractStateId = (int)ContractState.Initial;
+		ContractFund.ContractTypeId = 0;
+		return ContractFund;
 	}
-	public async Task<int> CreateContract(ContractParamModel ContractParam)
+	public async Task<Contract> CreateContract(ContractParamModel ContractParam)
 	{
+		var Contract = new Contract();
+/*
 		var ops = new List<Operation>();
 		if (ContractParam.Type == ContractPreTnxType.PreTxnAccountMerge) {
 			HorizonTimeBoundModel Time = new HorizonTimeBoundModel();
@@ -99,10 +107,9 @@ namespace Stellmart.Services
 			var pretxn2 = new ContractPreTxnModel();
 			pretxn2.XdrString = await _horizon.CreateTxn(ContractParam.EscrowAccount, ops, Time);
 			Contract.PreTransactions.Add(pretxn2);
-		} else {
-			return 0;
 		}
-		return 1;
+		*/
+		return Contract;
 	}
 
     public void UpdateContract(Contract contract)
@@ -113,10 +120,6 @@ namespace Stellmart.Services
 	public string SignContract(ContractSignatureModel signature)
 	{
 		string hash = "";
-		if(Contract.CurrentPhase != ContractPhaseType.NoPhase)
-		{
-			//todo
-		}
 		return hash;
 	}
 	public string ExecuteContract()
--- src/Services/Interfaces/IContractService.cs
+++ src/Services/Interfaces/IContractService.cs
@@ -1,6 +1,7 @@
 ﻿using Stellmart.Api.Data.Contract;
 using Stellmart.Api.Data.Horizon;
 using System.Threading.Tasks;
+using Stellmart.Api.Context.Entities;
 
 
 namespace Stellmart.Api.Services.Interfaces
@@ -11,11 +12,9 @@ namespace Stellmart.Api.Services.Interfaces
 	   Add signer as destination, stellmart and change threshold weights.
 	   Returns Escrow account id
          */
-	Task<int> SetupContract(ContractParamModel ContractParam);
-	/* Based on the Contract Parameters, Contract will be created.
-	   Returns Contract with one pre transaction added to the contract list
-	 */
-	Task<int> CreateContract(ContractParamModel ContractParam);
+	Task<Contract> SetupContract(ContractParamModel ContractParam);
+	 Task<Contract> FundContract(ContractParamModel ContractParam);
+	Task<Contract> CreateContract(ContractParamModel ContractParam);
 
 	string SignContract(ContractSignatureModel signature);
 	/*Submits the transaction to the network, returns the hash of transaction*/
