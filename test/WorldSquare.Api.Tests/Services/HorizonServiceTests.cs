ebf940d684dd20e6a95d55df4fb8b8a1afe4dce9
Amel Salibasic <nukecx@gmail.com>
2018-12-11 01:04:06 +0000

    get balance unit tests

--- test/WorldSquare.Api.Tests/Services/HorizonServiceTests.cs
+++ test/WorldSquare.Api.Tests/Services/HorizonServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.Extensions.Options;
 using NSubstitute;
 using stellar_dotnet_sdk;
@@ -6,36 +8,19 @@ using stellar_dotnet_sdk.responses;
 using Stellmart.Api.Business.Managers.Interfaces;
 using Stellmart.Api.Data.Horizon;
 using Stellmart.Api.Data.Settings;
+using Stellmart.Api.Services;
 using Stellmart.Api.Services.Interfaces;
-using Stellmart.Services;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace WorldSquare.Api.Tests.Services
 {
     public class HorizonServiceTests
     {
-        private IHorizonService _subjectUnderTest;
-        private readonly IOptions<HorizonSettings> _horizonSettingsMock;
-        private readonly IMapper _mapperMock;
-        private readonly IHorizonServerManager _horizonServerManagerMock;
-
-        private const string _horizonServer = "https://horizon-testnet.stellar.org/";
-
-        // generated secret seed with RNGCryptoServiceProvider and converted to base64string
-        private const string _secretSeed = "G3fQ8T/duAPYASsMnV7vwOdcXN2to7eA/FHkT9OEkvA=";
-        private byte[] _byteSecretSeed;
-
         public HorizonServiceTests()
         {
-            _horizonSettingsMock = Options.Create(new HorizonSettings()
-            {
-                Server = "testnet"
-            });
+            _horizonSettingsMock = Options.Create(new HorizonSettings {Server = "testnet"});
 
-            _byteSecretSeed = Convert.FromBase64String(_secretSeed);
+            _byteSecretSeed = Convert.FromBase64String(SECRET_SEED);
 
             _mapperMock = Substitute.For<IMapper>();
             _horizonServerManagerMock = Substitute.For<IHorizonServerManager>();
@@ -43,18 +28,24 @@ namespace WorldSquare.Api.Tests.Services
             _subjectUnderTest = new HorizonService(_horizonSettingsMock, _mapperMock, _horizonServerManagerMock);
         }
 
+        private readonly IHorizonService _subjectUnderTest;
+        private readonly IOptions<HorizonSettings> _horizonSettingsMock;
+        private readonly IMapper _mapperMock;
+        private readonly IHorizonServerManager _horizonServerManagerMock;
+
+        // generated secret seed with RNGCryptoServiceProvider and converted to base64string
+        private const string SECRET_SEED = "G3fQ8T/duAPYASsMnV7vwOdcXN2to7eA/FHkT9OEkvA=";
+        private readonly byte[] _byteSecretSeed;
+
         [Fact]
         public void CreateAccount_GetKeyPair()
         {
             var keyPair = KeyPair.FromSecretSeed(_byteSecretSeed);
 
-            var horizonKeyPairModel = new HorizonKeyPairModel
-            {
-                PublicKey = keyPair.AccountId,
-                SecretKey = keyPair.SecretSeed
-            };
-            
-            _horizonServerManagerMock.CreateAccount().Returns(horizonKeyPairModel);
+            var horizonKeyPairModel = new HorizonKeyPairModel {PublicKey = keyPair.AccountId, SecretKey = keyPair.SecretSeed};
+
+            _horizonServerManagerMock.CreateAccount()
+                .Returns(horizonKeyPairModel);
 
             var result = _subjectUnderTest.CreateAccount();
 
@@ -69,21 +60,27 @@ namespace WorldSquare.Api.Tests.Services
             var keyPair = KeyPair.FromSecretSeed(_byteSecretSeed);
 
             var accountResponse = new AccountResponse(keyPair)
-            {
-                KeyPair = keyPair,
-                Balances = new[]
-                {
-                    new Balance("XLM", "XLM", "xxx", "10000", "xxx", "xxx", "xxx")
-                }
-            };
+                                  {
+                                      KeyPair = keyPair,
+                                      Balances = new[]
+                                                 {
+                                                     new Balance(assetType: "native",
+                                                                 assetCode: "XLM",
+                                                                 assetIssuer: "xxx",
+                                                                 balance: "10000",
+                                                                 limit: "xxx",
+                                                                 buyingLiabilities: "xxx",
+                                                                 sellingLiabilities: "xxx")
+                                                 }
+                                  };
 
-            _mapperMock.Map<HorizonFundTestAccountModel>(Arg.Any<AccountResponse>()).Returns(new HorizonFundTestAccountModel()
-            {
-                PublicKey = accountResponse.KeyPair.AccountId
-            });
+            _mapperMock.Map<HorizonFundTestAccountModel>(Arg.Any<AccountResponse>())
+                .Returns(new HorizonFundTestAccountModel {PublicKey = accountResponse.KeyPair.AccountId});
 
-            _horizonServerManagerMock.FundTestAccountAsync(accountResponse.KeyPair.AccountId).Returns(Task.CompletedTask);
-            _horizonServerManagerMock.GetAccountAsync(accountResponse.KeyPair.AccountId).Returns(accountResponse);
+            _horizonServerManagerMock.FundTestAccountAsync(accountResponse.KeyPair.AccountId)
+                .Returns(Task.CompletedTask);
+            _horizonServerManagerMock.GetAccountAsync(accountResponse.KeyPair.AccountId)
+                .Returns(accountResponse);
 
             var account = await _subjectUnderTest.FundTestAccountAsync(keyPair.AccountId);
 
@@ -95,16 +92,93 @@ namespace WorldSquare.Api.Tests.Services
         {
             var keyPair = KeyPair.FromSecretSeed(_byteSecretSeed);
 
-            var accountResponse = new AccountResponse(keyPair)
-            {
-                SequenceNumber = 42
-            };
+            var accountResponse = new AccountResponse(keyPair) {SequenceNumber = 42};
 
-            _horizonServerManagerMock.GetAccountAsync(accountResponse.KeyPair.AccountId).Returns(accountResponse);
+            _horizonServerManagerMock.GetAccountAsync(accountResponse.KeyPair.AccountId)
+                .Returns(accountResponse);
 
             var result = await _subjectUnderTest.GetSequenceNumber(keyPair.AccountId);
 
             Assert.Equal(accountResponse.SequenceNumber, result);
+        }
+
+        [Fact]
+        public async Task GetAccountBalance_RequestBalanceWithAssetTypeNull_GetNativeBalance()
+        {
+            const string expectedBalance = "10000";
+
+            var keyPair = KeyPair.FromSecretSeed(_byteSecretSeed);
+
+            var assetModel = new HorizonAssetModel()
+                             {
+                                 AssetType = null,
+                                 AccountPublicKey = keyPair.AccountId
+                             };
+
+            var accountResponse = new AccountResponse(keyPair)
+                                  {
+                                      KeyPair = keyPair,
+                                      Balances = new[]
+                                                 {
+                                                     new Balance(assetType: "native",
+                                                                 assetCode: "XLM",
+                                                                 assetIssuer: "xxx",
+                                                                 balance: "10000",
+                                                                 limit: "xxx",
+                                                                 buyingLiabilities: "xxx",
+                                                                 sellingLiabilities: "xxx")
+                                                 }
+                                  };
+
+            _horizonServerManagerMock.GetAccountAsync(accountResponse.KeyPair.AccountId)
+                .Returns(accountResponse);
+
+            var result = await _subjectUnderTest.GetAccountBalance(assetModel);
+
+            Assert.Equal(expectedBalance, result);
+        }
+
+        [Fact]
+        public async Task GetAccountBalance_RequestBalanceWithAssetTypeWST_GetWSTBalance()
+        {
+            const string expectedBalance = "1234";
+            const string assetIssuerPublicKey = "GAEHO5MAKDWY3AHG6HCFIMTQT4XXTPFX6MWESLROASNHRPSW4NUR2F7Y";
+            var keyPair = KeyPair.FromSecretSeed(_byteSecretSeed);
+
+            var assetModel = new HorizonAssetModel()
+                             {
+                                 AssetCode = "WST",
+                                 AssetIssuerPublicKey = assetIssuerPublicKey
+            };
+
+            var accountResponse = new AccountResponse(keyPair)
+                                  {
+                                      KeyPair = keyPair,
+                                      Balances = new[]
+                                                 {
+                                                     new Balance(assetType: "native",
+                                                                 assetCode: "XLM",
+                                                                 assetIssuer: "xxx",
+                                                                 balance: "10000",
+                                                                 limit: "xxx",
+                                                                 buyingLiabilities: "xxx",
+                                                                 sellingLiabilities: "xxx"),
+                                                     new Balance(assetType: "alpha",
+                                                                 assetCode: "WST",
+                                                                 assetIssuer: assetIssuerPublicKey,
+                                                                 balance: "1234",
+                                                                 limit: "xxx",
+                                                                 buyingLiabilities: "xxx",
+                                                                 sellingLiabilities: "xxx")
+                                                 }
+                                  };
+
+            _horizonServerManagerMock.GetAccountAsync(Arg.Any<string>())
+                .Returns(accountResponse);
+
+            var result = await _subjectUnderTest.GetAccountBalance(assetModel);
+
+            Assert.Equal(expectedBalance, result);
         }
     }
 }
