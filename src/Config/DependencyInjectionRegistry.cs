806ceba813d8463b4fef1d18112e8806b23305a2
Amel Salibasic <nukecx@gmail.com>
2018-12-15 19:01:26 +0000

    make build buildable

--- src/Config/DependencyInjectionRegistry.cs
+++ src/Config/DependencyInjectionRegistry.cs
@@ -14,7 +14,6 @@ using Stellmart.Api.DataAccess;
 using Stellmart.Api.Services;
 using Stellmart.Api.Services.Interfaces;
 using Stellmart.Context;
-using Stellmart.Services;
 using StructureMap;
 using Yoti.Auth;
 
@@ -24,7 +23,7 @@ namespace Stellmart.Api.Config
     {
         public DependencyInjectionRegistry(IConfiguration configuration)
         {
-            Scan(x =>
+            Scan(action: x =>
             {
                 x.AssemblyContainingType<Startup>();
                 x.LookForRegistries();
@@ -33,40 +32,44 @@ namespace Stellmart.Api.Config
                 x.WithDefaultConventions();
             });
 
-            For<IUserStore<ApplicationUser>>().Use<ApplicationUserStore>();
-            For<UserManager<ApplicationUser>>().Use<UserManager<ApplicationUser>>();
-            For<IRepository>().Use<Repository<ApplicationDbContext>>();
-            For<IShippingService>().Singleton().Use<EasyPostService>();
-            For<IHttpContextAccessor>().Singleton().Use<HttpContextAccessor>();
-            For<IMapper>().Use(() => Mapper.Instance);
-            For<IHorizonService>().Singleton().Use<HorizonService>();
-            
+            For<IUserStore<ApplicationUser>>()
+                .Use<ApplicationUserStore>();
+            For<UserManager<ApplicationUser>>()
+                .Use<UserManager<ApplicationUser>>();
+            For<IRepository>()
+                .Use<Repository<ApplicationDbContext>>();
+            For<IShippingService>()
+                .Singleton()
+                .Use<EasyPostService>();
+            For<IHttpContextAccessor>()
+                .Singleton()
+                .Use<HttpContextAccessor>();
+            For<IMapper>()
+                .Use(expression: () => Mapper.Instance);
+            For<IHorizonService>()
+                .Singleton()
+                .Use<HorizonService>();
+
             For<Server>()
                 .Singleton()
-                .Use(new Server(configuration["HorizonSettings:Server"],
-                    new HttpClient
-                    {
-                        BaseAddress = new Uri(configuration["HorizonSettings:Server"])
-                    }
-                ));
+                .Use(new Server(configuration[key: "HorizonSettings:Server"], new HttpClient {BaseAddress = new Uri(configuration[key: "HorizonSettings:Server"])}));
 
             For<ISearchService>()
                 .Singleton()
-                .Use(new AzureSearchService(
-                    new SearchServiceClient(configuration["AzureSearch:ServiceName"],
-                        new SearchCredentials(configuration["AzureSearch:ApiKey"]))
-                ));
+                .Use(new AzureSearchService(new SearchServiceClient(configuration[key: "AzureSearch:ServiceName"],
+                                                                    new SearchCredentials(configuration[key: "AzureSearch:ApiKey"]))));
 
             For<YotiClient>()
                 .Singleton()
-                .Use(new YotiClient(configuration["YotiSettings:SdkId"],
-                    PemHelper.LoadPemFromString(configuration["YotiSettings:Pem"])));
+                .Use(new YotiClient(configuration[key: "YotiSettings:SdkId"], PemHelper.LoadPemFromString(configuration[key: "YotiSettings:Pem"])));
 
             var path = GeoIpHelper.GetGeoIpDatabaseFilename();
-            For<IGeoIP2DatabaseReader>().Singleton().Use(new DatabaseReader(path));
+            For<IGeoIP2DatabaseReader>()
+                .Singleton()
+                .Use(new DatabaseReader(path));
 
             // EasyPost
-            ClientManager.SetCurrent(configuration["EasyPost:ApiKey"]);
+            ClientManager.SetCurrent(configuration[key: "EasyPost:ApiKey"]);
         }
     }
 }
