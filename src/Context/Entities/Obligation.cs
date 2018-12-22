00a695f19aeb90f245538e446a4a3e9573966c94
Domenick Packett <Domenick.Packett@gmail.com>
2018-12-22 09:40:09 -0500

    Add Obligation entity

---
+++ src/Context/Entities/Obligation.cs
@@ -0,0 +1,23 @@
+﻿using Stellmart.Api.Context.Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stellmart.Api.Context.Entities
+{
+    public class Obligation : UniqueEntity
+    {
+        public int ServiceId { get; set; }
+        public int? ProviderId { get; set; }
+        public int RecipientId { get; set; }
+        public int? ArbiterId { get; set; }
+        public DateTime ServiceInitiationTimeLimit { get; set; }
+        public DateTime ServiceFulfillmentTimeLimit { get; set; }
+        public DateTime ServiceReceiptTimeLimit { get; set; }
+        public int IntermediaryPhases { get; set; }
+        public bool Fulfilled { get; set; }
+        public ICollection<Contract> ServiceContracts { get; set; }
+        public Contract CollateralContract { get; set; }
+    }
+}
